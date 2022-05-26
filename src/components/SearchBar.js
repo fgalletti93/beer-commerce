1d0c79b75b7210f2899d99da464b8d7d97fb351e
Francesca Galletti <francesca.galletti@pipefy.com>
2022-05-26 07:21:22 -0300

    Setting up base structure for app and searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Beer Search</label>
+          <input
+            type="text"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
