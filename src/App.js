20a9165cd5c24690da8eaaf9d0e47d8de15d5ce0
Francesca Galletti <francesca.galletti@pipefy.com>
2022-08-08 12:06:29 -0300

    create helper function for orderBy

--- src/App.js
+++ src/App.js
@@ -3,77 +3,25 @@ import { getBeers, getBeersByCustomSearch } from "./api/api";
 import CustomSearch from "./CustomSearch/CustomSearch";
 import ImageList from "./ImageList/ImageList";
 import "./styles/style.scss";
+import {orderBy} from "./utils/OrderBy";
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
- 
+
   const onTermSubmit = (term) => {
     getBeersByCustomSearch(term)
     .then((response) => response.json())
-    .then((data) => orderBy(data, term.order));
+    .then((data) => {
+      const results = orderBy(data, term.order)
+      setSearchResults(results)
+    }); 
   };
 
   useEffect(() => {
     getBeers()
       .then((response) => response.json())
       .then((data) => setSearchResults(data));
-  }, []); //passar props do customSearch
-
-  const orderBy = (data, option) => {
-    if (option === "A-Z") {
-      const alphabeticalOrder = data.sort((a, b) => {
-        const beerA = a.name.toUpperCase();
-        const beerB = b.name.toUpperCase();
-        if (beerA < beerB) {
-          return -1;
-        }
-        if (beerA > beerB) {
-          return 1;
-        }
-        return 0;
-      });
-      setSearchResults([...alphabeticalOrder]);
-    }
-    if (option === "Z-A") {
-      const ReverseAlphabeticalOrder = data.sort((a, b) => {
-        const beerA = a.name.toUpperCase();
-        const beerB = b.name.toUpperCase();
-        if (beerA < beerB) {
-          return 1;
-        }
-        if (beerA > beerB) {
-          return -1;
-        }
-        return 0;
-      });
-      setSearchResults([...ReverseAlphabeticalOrder]);
-    }
-    if (option === "Low to High ABV") {
-      const AbvLowToHigh = data.sort((a, b) => {
-        return a.abv - b.abv;
-      });
-      setSearchResults([...AbvLowToHigh]);
-    }
-    if (option === "High to Low ABV") {
-      const AbvHighToLow = data.sort((a, b) => {
-        return b.abv - a.abv;
-      });
-      setSearchResults([...AbvHighToLow]);
-    }
-    if (option === "Low to High IBU") {
-      const IbuLowToHigh = data.sort((a, b) => {
-        return a.ibu - b.ibu;
-      });
-      setSearchResults([...IbuLowToHigh]);
-    }
-    if (option === "High to Low IBU") {
-      const IbuHighToLow = data.sort((a, b) => {
-        return b.ibu - a.ibu;
-      });
-      setSearchResults([...IbuHighToLow]);
-    }
-  };
-
+  }, []); //custom hook that englobes search results
 
   return (
     <div>
---
+++ src/utils/OrderBy.js
@@ -0,0 +1,55 @@
+export const orderBy = (data, option) => {
+    if (option === "A-Z") {
+      const alphabeticalOrder = data.sort((a, b) => {
+        const beerA = a.name.toUpperCase();
+        const beerB = b.name.toUpperCase();
+        if (beerA < beerB) {
+          return -1;
+        }
+        if (beerA > beerB) {
+          return 1;
+        }
+        return 0;
+      });
+      return [...alphabeticalOrder];
+    }
+    if (option === "Z-A") {
+      const ReverseAlphabeticalOrder = data.sort((a, b) => {
+        const beerA = a.name.toUpperCase();
+        const beerB = b.name.toUpperCase();
+        if (beerA < beerB) {
+          return 1;
+        }
+        if (beerA > beerB) {
+          return -1;
+        }
+        return 0;
+      });
+      return [...ReverseAlphabeticalOrder];
+    }
+    if (option === "Low to High ABV") {
+      const AbvLowToHigh = data.sort((a, b) => {
+        return a.abv - b.abv;
+      });
+      return [...AbvLowToHigh];
+    }
+    if (option === "High to Low ABV") {
+      const AbvHighToLow = data.sort((a, b) => {
+        return b.abv - a.abv;
+      });
+      return [...AbvHighToLow];
+    }
+    if (option === "Low to High IBU") {
+      const IbuLowToHigh = data.sort((a, b) => {
+        return a.ibu - b.ibu;
+      });
+      return [...IbuLowToHigh];
+    }
+    if (option === "High to Low IBU") {
+      const IbuHighToLow = data.sort((a, b) => {
+        return b.ibu - a.ibu;
+      });
+      return [...IbuHighToLow];
+    }
+  };
+
