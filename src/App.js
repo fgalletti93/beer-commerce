b53fddf57129ef860785ef7726b14ce3d054ec17
Francesca Galletti <francesca.galletti@pipefy.com>
2022-08-20 21:33:36 -0300

    create custom hook for api fetch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { getBeers, getBeersByCustomSearch } from "./api/api";
+import React from "react";
 import CustomSearch from "./CustomSearch/CustomSearch";
 import ImageList from "./ImageList/ImageList";
+import useFetchBeers from "./utils/useFetchBeers";
 import "./styles/style.scss";
-import {orderBy} from "./utils/OrderBy";
+
 
 const App = () => {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const onTermSubmit = (term) => {
-    getBeersByCustomSearch(term)
-    .then((response) => response.json())
-    .then((data) => {
-      const results = orderBy(data, term.order)
-      setSearchResults(results)
-    }); 
-  };
-
-  useEffect(() => {
-    getBeers()
-      .then((response) => response.json())
-      .then((data) => setSearchResults(data));
-  }, []); //custom hook that englobes search results
-
+  const {searchResults, onSearchParamSubmit} = useFetchBeers();
+  
   return (
     <div>
       <CustomSearch
-        onCustomParamSubmit={onTermSubmit}
+        onCustomParamSubmit={onSearchParamSubmit}
       />
       <ImageList beers={searchResults} />
     </div>
---
+++ src/utils/useFetchBeers.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { getBeers, getBeersByCustomSearch } from "../api/api";
+import { orderBy } from "./OrderBy";
+
+const useFetchBeers = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const onSearchParamSubmit = (searchParam) => {
+    getBeersByCustomSearch(searchParam)
+      .then((response) => response.json())
+      .then((data) => {
+        const results = orderBy(data, searchParam.order);
+        setSearchResults(results);
+      });
+  };
+
+  useEffect(() => {
+    getBeers()
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data));
+  }, []);
+
+  return {
+    searchResults,
+    onSearchParamSubmit,
+  };
+};
+
+export default useFetchBeers;
