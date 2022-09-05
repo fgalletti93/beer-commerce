27198b3ebc7991f311a66871402595ec8a83b261
Francesca Galletti <francesca.galletti@pipefy.com>
2022-09-05 11:55:56 -0300

    add default image when image url returns null

--- src/ImageList/ImageList.js
+++ src/ImageList/ImageList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.scss";
 import selected from "../assets/AdobeStock_303721216.png";
+import defaultImg from "../assets/defaultBeer.jpeg"
 
 const ImageList = ({ beers }) => {
   const card = beers.map((card) => {
     return (
       <div className="card" key={card.id}>
         <div className="card-content">
-          <img className="image" src={card.image_url} alt={card.abv} />
+          <img className="image" src={card.image_url || defaultImg} alt={card.abv} />
           <div className="text-content">
             <h1 className="title" data-testid="title">
               {card.name}
@@ -24,3 +25,7 @@ const ImageList = ({ beers }) => {
 };
 
 export default ImageList;
+
+
+//on click image, abrir outra pagina usando react router DOM 
+//adicionar  || para image source para ter imagem default quando image_url vier null 
